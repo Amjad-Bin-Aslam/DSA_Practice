737b7e4290959b0b236b7c37e5f213f490796844
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-12-03 00:52:01 +0500

    added

---
+++ array.js
@@ -0,0 +1,16 @@
+let arr = [1, 2, 3, 4, 5];
+let arr2 = [10, 20, 30]
+
+// arr.push(4)
+// arr.pop()
+
+
+// arr.unshift(10)
+// console.log(arr);
+// arr.shift()
+
+
+
+
+
+// let arr3 = arr.concat(arr2)
