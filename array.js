6acef978bb927e210d3c75e2f5ac1b4aaf10e2d0
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-12-03 02:07:18 +0500

    added

--- array.js
+++ array.js
@@ -8,9 +8,56 @@ let arr2 = [10, 20, 30]
 // arr.unshift(10)
 // console.log(arr);
 // arr.shift()
+// console.log(arr.entries())
+
+// Create an Array
+// let fruits = ["apple", "grape", "banana", "mango"]
+// let text = ""
+// for (let x of fruits.entries()) {
+//     text += x
+// }
+// console.log(text)
 
 
-
+// let i = 0
+// do {
+//     console.log(arr[i])
+//     i++
+// } while (i < arr.length);
 
 
 // let arr3 = arr.concat(arr2)
+
+
+// const mapArr = arr.map((item, index, array) => {
+//     return item + 20
+// })
+// console.log(mapArr)
+// console.log(arr)
+
+
+// const filterArr = arr.filter((item) => {
+//     return item >= 2
+// })
+// console.log(filterArr)
+// console.log(arr)
+
+
+
+// const sum = arr.reduce((prev, item) => {
+//     console.log(prev)
+//     return prev + item
+// }, 0)
+
+// console.log(sum)
+
+
+// const arr1 = ['a', 'b', 'c', 'd']
+// arr1.splice(1, 3, 'h', 'g')
+// console.log(arr1)
+
+
+// const num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// const num2 = num.slice(0, 5)
+// console.log(num)
+// console.log(num2)
