a2d0d97b23086140d43e851a3b2f978a4dfa9d6c
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-12-04 01:13:07 +0500

    added

--- array.js
+++ array.js
@@ -136,19 +136,36 @@ let array = [10, 3, 45, 67, 8, 9, 7, 6, 4, 5, 6, 7, 8]
 
 // optimized way
 // [10, 3, 45, 67, 8, 9, 7, 6, 4, 5, 6, 7, 8]
-function secondLargestNumber(arr) {
-    let largest = Number.NEGATIVE_INFINITY;
-    let secondLargest = Number.NEGATIVE_INFINITY;
+// function secondLargestNumber(arr) {
+//     let largest = Number.NEGATIVE_INFINITY;
+//     let secondLargest = Number.NEGATIVE_INFINITY;
 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > largest) {
-            secondLargest = largest
-            largest = arr[i]
-        } else if (arr[i] != largest && arr[i] > secondLargest) {
-            secondLargest = arr[i]
-        }
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] > largest) {
+//             secondLargest = largest
+//             largest = arr[i]
+//         } else if (arr[i] != largest && arr[i] > secondLargest) {
+//             secondLargest = arr[i]
+//         }
+//     }
+//     return secondLargest;
+// }
+
+// console.log(secondLargestNumber(array))
+
+
+// [1,2,3,4,5,6,7,8,9]
+function rotateArrya(arr, k) {
+    let size = arr.length
+
+    if (k > size) {
+        k = k % size
     }
-    return secondLargest;
+
+    const rotated = arr.splice(size - k, size) //[6,7,8]
+    arr.unshift(...rotated)
+
+    return arr;
 }
 
-console.log(secondLargestNumber(array))
+console.log(rotateArrya([1, 2, 3, 4, 5, 6, 7, 8, 9], 8))
