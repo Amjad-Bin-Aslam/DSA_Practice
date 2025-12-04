1c5b809860887b531ce6bd33b878edf1202eae69
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-12-05 01:15:31 +0500

    added

--- array.js
+++ array.js
@@ -155,17 +155,56 @@ let array = [10, 3, 45, 67, 8, 9, 7, 6, 4, 5, 6, 7, 8]
 
 
 // [1,2,3,4,5,6,7,8,9]
-function rotateArrya(arr, k) {
-    let size = arr.length
+// function rotateArrya(arr, k) {
+//     let size = arr.length
 
-    if (k > size) {
-        k = k % size
+//     if (k > size) {
+//         k = k % size
+//     }
+
+//     const rotated = arr.splice(size - k, size) //[6,7,8]
+//     arr.unshift(...rotated)
+
+//     return arr;
+// }
+// console.log(rotateArrya([1, 2, 3, 4, 5, 6, 7, 8, 9], 8))
+
+
+
+// fabonacci algortihm 
+// let prev1 = 0, prev2 = 1;
+// for (let i = 0; i <= 18; i++) {
+//     let fabonacciNumber = prev1 + prev2
+//     console.log(`Fabo ${i}:`, fabonacciNumber)
+//     prev1 = prev2
+//     prev2 = fabonacciNumber
+// }
+
+// fabonacci series = 0,1,1,2,3,5,8,13,21,34,55,89,144,233
+// 1st iteration
+// index = 2, value = 1, so 1+0 = 1 and arr = [0,1,1]
+// 2nd iteration
+// index = 3, value = 1, so 1+1 = 2 and arr = [0,1,1,2]
+// 3rd iteration
+// index = 4, value = 2, so 2+1 = 3 and arr = [0,1,1,2,3]
+// 4th iteration
+// index = 5, value = 3, so 3+2 = 5 and arr = [0,1,1,2,3,5]
+
+var fib = function (n) {
+    const arr = [0, 1]
+
+    for (i = 2; i <= n; i++) {
+        arr.push(arr[i - 1] + arr[i - 2])
     }
-
-    const rotated = arr.splice(size - k, size) //[6,7,8]
-    arr.unshift(...rotated)
-
-    return arr;
+    console.log(arr[n])
 }
 
-console.log(rotateArrya([1, 2, 3, 4, 5, 6, 7, 8, 9], 8))
+fib(5)
+
+
+
+
+// const isPlindrome = (x) => {
+//     return x < 0 ? false : x === +x.toString().split('').reverse().join('')
+// }
+// console.log(isPlindrome(154)) 
